3b7f767fd6abfb857d677496fb6a7520aec97b50
ruslan <rus-h-903@mail.ru>
2022-09-26 16:32:34 +0300

    game and tests

---
+++ 01/main.py
@@ -0,0 +1,86 @@
+class TicTacGame:
+    def __init__(self):
+        self.cells = ["_"] * 9
+        self.letters = ['a', 'b', 'c']
+        self.move = 'X'
+
+    def show_board(self):
+        print("  a  b  c")
+        for i in range(3):
+            print(f"{i + 1} ", end='')
+            for j in range(3):
+                print(f"{self.cells[i * 3 + j]}  ", end='')
+            print('')
+
+    def validate_input(self, text):
+        try:
+            let, num = text
+        except ValueError:
+            return -1
+        try:
+            num = int(num)
+        except ValueError:
+            return -2
+        if let not in self.letters or not 0 < num < 4:
+            return -3
+        index = self.letters.index(let) + (num - 1) * 3
+        if self.cells[index] != '_':
+            return -4
+        return index + 1
+
+    def start_game(self):
+        count = 0
+        work = True
+        self.show_board()
+        while work and count < 9:
+            index = False
+            while index < 1:
+                text = input("Введите номер ячейки вида 'a1'\n")
+                index = self.validate_input(text)
+                self.print_err(index)
+            index -= 1
+            self.cells[index] = 'X' if count % 2 == 0 else '0'
+            self.move = self.cells[index]
+            self.show_board()
+            work = not self.check_winner()
+            count += 1
+        if not work:
+            print(f"Игра окончена. \"{self.move}\" победили")
+        else:
+            print("Игра окончена. Ничья")
+
+    def check_winner(self):
+        result = False
+        for j in range(9):
+            if not self.equal(j):
+                continue
+            for k in range(j + 1, 9):
+                diff = k - j
+                if self.equal(k) and (diff + k) < 9 and self.equal(diff + k):
+                    if diff == 1 and j % 3 == 0:
+                        result = True
+                    elif diff == 2 and j == 2:
+                        result = True
+                    elif diff == 3:
+                        result = True
+        return result
+
+    def equal(self, index):
+        return self.cells[index] == self.move
+
+    def print_err(self, err_num):
+        match err_num:
+            case -1:
+                print("Неверное количество символов")
+            case -2:
+                print("Введен символ вместа числа")
+            case -3:
+                print("Такой ячейки не существует")
+            case -4:
+                print("Данная ячейка занята")
+
+
+if __name__ == "__main__":
+    GAME = TicTacGame()
+    GAME.start_game()
+
---
+++ 01/tests.py
@@ -0,0 +1,55 @@
+import unittest
+import io
+import sys
+from main import TicTacGame
+
+
+class TestGame(unittest.TestCase):
+
+    def test_validate_input(self):
+        game = TicTacGame()
+        self.assertTrue(game.validate_input('a1'))
+        self.assertEqual(game.validate_input('a1'), 1)
+        self.assertEqual(game.validate_input('b2'), 5)
+        self.assertEqual(game.validate_input('c3'), 9)
+        game.cells = ['_', '_', '_', '_', '_', '_', '_', '_', 'X']
+        self.assertEqual(game.validate_input('c3'), -4)
+        self.assertEqual(game.validate_input('c3c'), -1)
+        self.assertEqual(game.validate_input('cc'), -2)
+        self.assertEqual(game.validate_input('c4'), -3)
+
+    def test_check_winner(self):
+        game = TicTacGame()
+        game.move = 'X'
+        game.cells = ['X', '0', 'X', '0', 'X', '0', 'X', '_', '_']
+        self.assertTrue(game.check_winner())
+        game.cells = ['X', 'X', '0', '0', 'X', '0', '0', 'X', '_']
+        self.assertTrue(game.check_winner())
+        game.cells = ['X', 'X', '0', '0', 'X', 'X', 'X', '0', '0']
+        self.assertFalse(game.check_winner())
+        game.cells = ['X', 'X', 'X', '0', '_', '0', 'X', '_', '0']
+        self.assertTrue(game.check_winner())
+        game.cells = ['X', '0', 'X', '0', 'X', '0', 'X', '_', '_']
+        game.move = '0'
+        self.assertFalse(game.check_winner())
+
+    def test_print(self):
+        exceptions = [
+            "Неверное количество символов\n",
+            "Введен символ вместа числа\n",
+            "Такой ячейки не существует\n",
+            "Данная ячейка занята\n"
+        ]
+        self.assertEqual(self.get_stdout(-1).getvalue(), exceptions[0])
+        self.assertEqual(self.get_stdout(-2).getvalue(), exceptions[1])
+        self.assertEqual(self.get_stdout(-3).getvalue(), exceptions[2])
+        self.assertEqual(self.get_stdout(-4).getvalue(), exceptions[3])
+
+    def get_stdout(self, err):
+        game = TicTacGame()
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        game.print_err(err)
+        sys.stdout = sys.__stdout__
+        return captured_output
+
